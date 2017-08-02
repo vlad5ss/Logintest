064fe3e6efa2f5f9cdf6a6c5ce199ae9c43b6fdc
vlad5ss <vlad5grup@gmail.com>
2017-08-02 21:55:48 +0400

    Added ViewModel

--- Logintest/LoginViewController.swift
+++ Logintest/LoginViewController.swift
@@ -7,7 +7,8 @@
 //
 import UIKit
 class LoginViewController: UIViewController {
-    
+    var viewModel:LoginViewModel?
+
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -15,25 +16,78 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if self.viewModel == nil {
+            self.viewModel = LoginViewModel(view: self)
+        }
+        
+        self.viewModel?.performInitialViewSetup()
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     @IBAction func login(_ sender: Any) {
+        viewModel?.login(userName: userNameTextField.text, password: passwordTextField.text)
+        
     }
     @IBAction func createAccount(_ sender: Any) {
+        self.performSegue(withIdentifier: "presentCreateAccount", sender: self)
+        
     }
     @IBAction func userNameDidEndOnExit(_ sender: Any) {
+        viewModel?.userNameDidEndOnExit()
+
+        
     }
     @IBAction func passwordDidEndOnExit(_ sender: Any) {
+        viewModel?.passwordDidEndOnExit()
     }
 }
 
 
 
 extension LoginViewController: UITextFieldDelegate {
+    
     func textField(_ textField: UITextField,
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
-        return true }
+        
+        if textField == self.userNameTextField {
+            self.viewModel?.userNameUpdated(textField.text)
+        }
+        
+        if textField == self.passwordTextField {
+            self.viewModel?.passwordUpdated(textField.text)
+        }
+        
+        return true
+    }
+    
 }
+
+
+extension LoginViewController : LoginViewControllerProtocol {
+    
+    func clearUserNameField() {
+        self.userNameTextField.text = ""
+    }
+    
+    func clearPasswordField() {
+        self.passwordTextField.text = ""
+    }
+    
+    func enableLoginButton(_ status:Bool) {
+        self.loginButton.isEnabled = status
+    }
+    
+    func enableCreateAccountButton(_ status:Bool) {
+        self.loginButton.isEnabled = status
+    }
+    
+    func hideKeyboard() {
+        self.userNameTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
+    }
+}
+
--- Logintest/SignupViewController.swift
+++ Logintest/SignupViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 class SignupViewController: UIViewController {
+    
+    var viewModel:SignupViewModel?
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
@@ -16,27 +19,92 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if self.viewModel == nil {
+            self.viewModel = SignupViewModel(view: self)
+        }
+        
+        self.viewModel?.performInitialViewSetup()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     @IBAction func create(_ sender: Any) {
+         viewModel?.signup(userName: userNameTextField.text, password: passwordTextField.text, emailAddress: emailAddressTextField.text)
     }
     @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
 }
 
     @IBAction func userNameDidEndOnExit(_ sender: Any) {
+        viewModel?.userNameDidEndOnExit()
     }
     @IBAction func passwordDidEndOnExit(_ sender: Any) {
+        viewModel?.passwordDidEndOnExit()
     }
     @IBAction func confirmPasswordDidEndOnExit(_ sender: Any) {
+         viewModel?.confirmPasswordDidEndOnExit()
     }
     @IBAction func emailAddressDidEndOnExit(_ sender: Any) {
+         viewModel?.emailAddressDidEndOnExit()
     }
 }
+
+
 extension SignupViewController: UITextFieldDelegate {
+    
     func textField(_ textField: UITextField,
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
-        return true }
+        
+        if textField == self.userNameTextField {
+            self.viewModel?.userNameUpdated(textField.text)
+        }
+        
+        if textField == self.passwordTextField {
+            self.viewModel?.passwordUpdated(textField.text)
+        }
+        
+        if textField == self.confirmPasswordTextField {
+            self.viewModel?.confirmPasswordUpdated(textField.text)
+        }
+        
+        if textField == self.emailAddressTextField {
+            self.viewModel?.emailAddressUpdated(textField.text)
+        }
+        
+        return true
+    }
+    
 }
+
+extension SignupViewController : SignupViewControllerProtocol {
+    
+    func clearUserNameField() {
+        self.userNameTextField.text = ""
+    }
+    
+    func clearPasswordField() {
+        self.passwordTextField.text = ""
+    }
+    
+    func clearConfirmPasswordField() {
+        self.confirmPasswordTextField.text = ""
+    }
+    
+    
+    func enableCancelButton(_ status:Bool) {
+        self.cancelButton.isEnabled = status
+    }
+    
+    func enableCreateButton(_ status:Bool) {
+        self.createButton.isEnabled = status
+    }
+    
+    func hideKeyboard() {
+        self.userNameTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
+        self.confirmPasswordTextField.resignFirstResponder()
+    }
+}
+
+
